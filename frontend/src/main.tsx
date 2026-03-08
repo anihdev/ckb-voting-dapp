0d6b14cece5ed2de573340ca89137a62f43ce260
summer <anihwisdom6@gmail.com>
2026-03-09 00:06:17 +0100

    refactoring_voting logic

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,15 @@
-// /**
-//  * Frontend Entry Point
-//  * File: frontend/src/main.tsx
-//  */
+/**
+ * Frontend Entry Point
+ * File: frontend/src/main.tsx
+ */
 
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
